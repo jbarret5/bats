26ec9bf97e5bde7f04148717fb64eabe36dd77ba
jbarret5 <jbarret5@confederationcollege.ca>
2023-03-16 15:54:33 -0400

    V2

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
+     eleventyConfig.addPlugin(eleventyNavigationPlugin);
      eleventyConfig.addPassthroughCopy('css')
      return {
           passthroughFileCopy: true
      }
 }
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
